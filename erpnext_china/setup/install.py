74e5d13440de72a62b1a455a126d4eeaec7adef6
NolanZONG <zongnan1989@gmail.com>
2026-07-02 11:42:39 +0900

    fix icon

--- erpnext_china/setup/install.py
+++ erpnext_china/setup/install.py
@@ -138,6 +138,8 @@ def set_v16_icon():
                     "logo_url": "/assets/erpnext_china/icons/desktop_icons/solid/cn_account_report.svg",
                 },
             )
+        else:
+			frappe.log_error("erpnext_china set_v16_icon failed: 'Desktop Icon' table does not exist.")
 
         # 2. 更新 Workflow Sidebar：中国财务报表
         workflow_sidebar_name = "中国财务报表"
@@ -148,5 +150,7 @@ def set_v16_icon():
                 "header_icon",
                 "cn-account-reporting"
             )
+        else:
+			frappe.log_error("erpnext_china set_v16_icon failed: 'Workspace Sidebar' table does not exist.")
     except Exception as e:
         frappe.log_error(f"erpnext_china set_v16_icon failed: {str(e)}")
